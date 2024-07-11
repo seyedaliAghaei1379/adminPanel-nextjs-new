9584b978cbff4383507cdb2d1e107c69f0b82e0d
ali <seyedali1379aghaei@gmail.com>
2024-07-10 17:54:00 -0700

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: process.env.NODE_ENV === "production" ? "/adminPanel-nextjs-new" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/adminPanel-nextjs-new/" : "",
+};
 
 export default nextConfig;
