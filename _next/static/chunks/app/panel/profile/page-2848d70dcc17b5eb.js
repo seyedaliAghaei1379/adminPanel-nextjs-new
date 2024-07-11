5938ed71ccc6f0feca4be842b088c136147aaf7e
ali <seyedali1379aghaei@gmail.com>
2024-07-10 18:27:16 -0700

    Updates

---
+++ _next/static/chunks/app/panel/profile/page-2848d70dcc17b5eb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[138,271,98,133,737],{3114:function(e,n,s){Promise.resolve().then(s.t.bind(s,1749,23)),Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,2553))}},function(e){e.O(0,[250,134,553,971,69,744],function(){return e(e.s=3114)}),_N_E=e.O()}]);
