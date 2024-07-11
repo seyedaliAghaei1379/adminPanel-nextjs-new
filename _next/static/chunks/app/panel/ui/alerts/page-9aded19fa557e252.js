e64af82f5991c0fe0f35066c5db54ea8339200f3
ali <seyedali1379aghaei@gmail.com>
2024-07-10 18:05:17 -0700

    Updates

--- _next/static/chunks/app/panel/ui/alerts/page-9aded19fa557e252.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[263,269,153],{679:function(e,n,s){Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,2553))}},function(e){e.O(0,[134,553,971,69,744],function(){return e(e.s=679)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/panel/ui/buttons/page-70484a2f9ea5b315.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[153,269,263],{679:function(e,n,s){Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,2553))}},function(e){e.O(0,[250,134,553,971,69,744],function(){return e(e.s=679)}),_N_E=e.O()}]);
