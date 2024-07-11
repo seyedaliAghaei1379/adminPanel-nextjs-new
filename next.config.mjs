ea325335af65648c1fcb1a76c9db1e96bcfe27f3
ali <seyedali1379aghaei@gmail.com>
2024-07-10 18:08:56 -0700

    third commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === "production" ? "/adminPanel-nextjs-new" : "",
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/adminPanel-nextjs-new" : "",
   assetPrefix:
     process.env.NODE_ENV === "production" ? "/adminPanel-nextjs-new/" : "",
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h4>salam</h4>;
+  return (
+    <div className="bg-zinc-300 flex justify-center items-center">
+      <Link href={"/panel"} className="text-white bg-slate-600 p-4 rounded-xl ">go to panel</Link>
+    </div>
+  );
 }
